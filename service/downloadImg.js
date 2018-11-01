453b899af806e702c9d6e72b2f25b4635735567b
642250185 <nihao642250185@163.com>
2018-11-01 11:52:53 +0800

    pirate

--- service/downloadImg.js
+++ service/downloadImg.js
@@ -9,7 +9,7 @@ const detailsPath = config.detailsPath;
 
 
 let count = 0;
-const downloadImages = async(isbn, title, src) => {
+const downloadImages = async(channel, isbn, title, src) => {
     try {
         ++count;
         const imgs = src.split("、");
@@ -17,6 +17,11 @@ const downloadImages = async(isbn, title, src) => {
         for(let url of imgs){
             await sleep(1000 * 2);
             const uuidStr = uuid();
+            if(channel === 0){  // 当当官网替换成大图地址。
+                const forepart = url.substring(0, url.indexOf("_")+1);
+                const backEnd = url.substring(url.lastIndexOf("_"), url.length);
+                url = `${forepart}u${backEnd}`;
+            }
             const fileName = `${isbn}-${uuidStr}`;
             const path = `${imagesPath}/${fileName}.jpeg`;
             await request(url).pipe(fs.createWriteStream(path)).on('close', () =>{
@@ -32,11 +37,16 @@ const downloadImages = async(isbn, title, src) => {
 
 const saveAllImages = async() => {
     try {
+        let index = 0;
         const details = JSON.parse(fs.readFileSync(detailsPath));
         console.info(`共 ${details.length} 本书准备下载图片......`);
         for(let item of details){
-            const {isbn, title, src} = item;
-            await downloadImages(isbn, title, src);
+            ++index;
+            const {channel, isbn, title, src} = item;
+            await downloadImages(channel, isbn, title, src);
+            // if(index === 15){
+            //     break;
+            // }
         }
     } catch (e) {
         console.error(e);
